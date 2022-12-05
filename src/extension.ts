d665e0269bd2f1592510ab8a251bf6971922e2e4
Kiran <116376429+kiranvshah@users.noreply.github.com>
2022-12-05 20:52:28 +0000

    remove unnecessary async

--- src/extension.ts
+++ src/extension.ts
@@ -82,7 +82,7 @@ const sendQueryToChatGPT = async (queryText: string) => {
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	const askChatGptWhyNotWorking = vscode.commands.registerCommand('chatgpt-helper.askChatGptWhyNotWorking', async () => {
+	const askChatGptWhyNotWorking = vscode.commands.registerCommand('chatgpt-helper.askChatGptWhyNotWorking', () => {
 		// code placed here will be executed every time command is executed
 		
 		// store selected code in a variable named selectedCode
