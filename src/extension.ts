bcc17b47ffd0b32a8aabd5d30a6036100d14571b
Kiran <116376429+kiranvshah@users.noreply.github.com>
2022-12-18 14:47:06 +0000

    use browser

--- src/extension.ts
+++ src/extension.ts
@@ -2,13 +2,55 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode"
 import { ChatGPTAPI } from "chatgpt"
+import puppeteer from "puppeteer"
+
+interface GetOpenAIAuthResponse {
+	sessionToken?: string
+	clearanceToken: string
+}
+const getOpenAIAuth = async () => {
+	const browser = await puppeteer.launch({
+		userDataDir: "/chatgpt-helper/chromedata",
+		headless: false, // so user can get past cloudflare
+	}) // change headless to false to see the browser
+	const page = await browser.newPage()
+
+	await page.goto("https://chat.openai.com/chat")
+
+	if (page.url() === "https://chat.openai.com/auth/login") {
+		// make user login themselves in chrome window
+	}
+
+	console.log(page.cookies())
+
+	return {} as GetOpenAIAuthResponse
+}
 
 const sendQueryToChatGPT = async (queryText: string) => {
 	// get user or workspace configuration object
 	let config = vscode.workspace.getConfiguration()
 
 	const getSessionToken = () =>
-		config.get("chatgptHelper.sessionToken") as string
+		config.get("chatgptHelper.openaiAuth.sessionToken") as string
+
+	const getClearanceToken = () =>
+		config.get("chatgptHelper.openaiAuth.clearanceToken") as string
+
+	const updateBothAuthTokens = async () => {
+		const openAIAuth = (await getOpenAIAuth()) as GetOpenAIAuthResponse
+		config.update(
+			"chatgptHelper.openaiAuth.sessionToken",
+			openAIAuth.sessionToken,
+		)
+		config.update(
+			"chatgptHelper.openaiAuth.clearanceToken",
+			openAIAuth.clearanceToken,
+		)
+		config = vscode.workspace.getConfiguration()
+	}
+
+	const updateClearanceTokenOnly = async () =>
+		((await getOpenAIAuth()) as GetOpenAIAuthResponse).clearanceToken
 
 	const outputDocument = await vscode.workspace.openTextDocument({
 		content: "Querying ChatGPT. This may take some time...",
@@ -26,47 +68,51 @@ const sendQueryToChatGPT = async (queryText: string) => {
 	const entireQueryText = queryText.split("\r\n").join("\n")
 
 	if (!getSessionToken()) {
-		// update session token
-		await config.update(
-			"chatgptHelper.sessionToken",
-			await vscode.window.showInputBox({
-				title: "ChatGPT Session Token",
-				prompt: "Go to chat.openai.com/chat. Press Ctrl+Shift+I (or Command+Option+I on Mac). Open Applications > Cookies > https://chat.openai.com. Copy the value of the `__Secure-next-auth.session-token` and paste it here.",
-				placeHolder: "ChatGPT Session Token",
-				ignoreFocusOut: true,
-			}),
-			vscode.ConfigurationTarget.Global,
-		)
-		// update config var
-		config = vscode.workspace.getConfiguration()
+		await updateBothAuthTokens()
+	} else if (!getClearanceToken()) {
+		await updateClearanceTokenOnly()
 	}
+	// if clearance token is invalid, the value will still be there so we will find through the try/catch
 
-	try {
-		// query chatgpt
-		const api = new ChatGPTAPI({ sessionToken: getSessionToken() })
-		await api.ensureAuth()
+	// runs twice: if caught the first time, clearance token will be updated
+	for (const runNum of [1, 2]) {
+		try {
+			// query chatgpt
+			const api = new ChatGPTAPI({
+				sessionToken: getSessionToken(),
+				clearanceToken: getClearanceToken(),
+			})
+			await api.ensureAuth()
 
-		const response = await api.sendMessage(entireQueryText)
+			const response = await api.sendMessage(entireQueryText)
 
-		outputDocumentEditor.edit(editBuilder => {
-			editBuilder.replace(
-				new vscode.Range(
-					new vscode.Position(0, 0),
-					new vscode.Position(99999999999999, 0),
-				),
-				response,
-			)
-		})
-	} catch (error) {
-		outputDocumentEditor.edit(editBuilder => {
-			editBuilder.replace(
-				new vscode.Range(
-					new vscode.Position(0, 0),
-					new vscode.Position(99999999999999, 0),
-				),
-				`Error querying ChatGPT. Try updating your session token?\n\n${error}`,
-			)
-		})
+			outputDocumentEditor.edit(editBuilder => {
+				editBuilder.replace(
+					new vscode.Range(
+						new vscode.Position(0, 0),
+						new vscode.Position(99999999999999, 0),
+					),
+					response,
+				)
+			})
+			return // success - dont run loop again!
+		} catch (error) {
+			if (runNum === 1) {
+				// first time running: let's try updating the clearance token
+				await updateClearanceTokenOnly()
+			} else {
+				// second time running: tell the user something went wrong
+				outputDocumentEditor.edit(editBuilder => {
+					editBuilder.replace(
+						new vscode.Range(
+							new vscode.Position(0, 0),
+							new vscode.Position(99999999999999, 0),
+						),
+						`Error querying ChatGPT. Try updating your session token?\n\n${error}`,
+					)
+				})
+			}
+		}
 	}
 }
 
